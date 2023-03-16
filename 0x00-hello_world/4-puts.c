06a1bfbd907231cbe4a405a28282fe384a27117e
Ranimulu <ranyanurhussen@gmail.com>
2023-03-16 01:54:17 -0700

    TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - is the main entry point
- * return - always 0 is success
+ * return - always 0 is a success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
