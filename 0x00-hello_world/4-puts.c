0ff91c190e7592f20e07da6354da69c515d9df59
Ranimulu <ranyanurhussen@gmail.com>
2023-03-16 01:46:08 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - is the main entry point
+ * return - always 0 is success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
