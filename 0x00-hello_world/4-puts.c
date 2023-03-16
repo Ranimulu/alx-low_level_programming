906ceb1abc2a446dbbee2beeb7f7b4126234d802
Ranimulu <ranyanurhussen@gmail.com>
2023-03-16 09:08:19 -0700

    TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - is a program that prints a line with puts function
  *
- * return: 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
