b04bc17f7c15950f0ceacff079ff9a70966da790
Ranimulu <ranyanurhussen@gmail.com>
2023-03-24 13:11:09 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - shows uppercase letters
+ * @c: to be enetered
+ * Return: 1 if uppercase and 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
